96955a81d1dbd9b06b03fea2cbe23182fe662197
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-30 20:37:45 +0500

    created user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4FRdJhLdEEhFbcIuR-DEGYsFMjn-6QUGZ7A&usqp=CAU',
+    },
+  },
+  { timestamps: true },
+);
+const User = mongoose.model('User', userModel);
+module.exports = User;
