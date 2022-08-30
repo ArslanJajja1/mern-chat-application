312d168c72f5affb5f9159f491e9ba48dd50b5d1
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-30 10:43:59 +0500

    initial setup,created node.js server and express api setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const { chats } = require('./data/data');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Express API is running ');
+});
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+console.log(process.env.PORT);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log('Server is running on port ', PORT));
