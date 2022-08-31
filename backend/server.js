972095fb1e5cf963d78c58b5774d31b214e3d9fd
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 20:19:38 +0500

    parsing json data,some indentation fixes

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,14 @@ const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
 const { chats } = require('./data/data');
-const app = express();
+const userRoutes = require('./routes/userRoutes');
 connectDB();
+const app = express();
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Express API is running ');
 });
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
-});
-app.get('/api/chat/:id', (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('/api/user', userRoutes);
 console.log(process.env.PORT);
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log('Server is running on port ', PORT));
