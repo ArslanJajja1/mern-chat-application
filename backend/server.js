4e75e5836c4d0e767310d02080ccbf764bb0e771
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 08:29:28 +0500

    mongoDB connected with node/express backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
 const { chats } = require('./data/data');
 const app = express();
+connectDB();
 app.get('/', (req, res) => {
   res.send('Express API is running ');
 });
