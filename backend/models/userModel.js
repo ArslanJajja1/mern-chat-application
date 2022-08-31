6213b1178f33fabaa61c03748c3b7bff88608b64
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 21:04:16 +0500

    password encryption error fix,checking password match

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -22,11 +22,15 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true },
 );
-userSchema.pre('save',function(next){
-if(!this.modified){
-   const salt = await bcrypt.genSalt(10)
-   this.password = await bcrypt.hash(this.password,salt)
-}
-})
+userSchema.pre('save', async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model('User', userSchema);
 module.exports = User;
