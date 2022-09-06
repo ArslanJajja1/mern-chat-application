b96ad87fd9d4514ff4fcbd2f5514fad34453b77b
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-06 20:40:49 +0500

    access/create and fetch chats

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { chats } = require('./data/data');
 const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 connectDB();
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
   res.send('Express API is running ');
 });
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 8000;
