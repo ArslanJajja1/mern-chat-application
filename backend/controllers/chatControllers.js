20fa1a81ee68844db900c044d9e6cbe8371c6701
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-13 20:22:07 +0500

    rename group controller

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -76,7 +76,19 @@ const createGroupChat = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
-const renameGroup = asyncHandler(async (req, res) => {});
+const renameGroup = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+  const updatedChat = await Chat.findByIdAndUpdate(chatId, { chatName }, { new: true })
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error('Chat Not Found');
+  } else {
+    res.json(updatedChat);
+  }
+});
+
 const removeFromGroup = asyncHandler(async (req, res) => {});
 const addToGroup = asyncHandler(async (req, res) => {});
 module.exports = { accessChat, fetchChats, createGroupChat, renameGroup, removeFromGroup, addToGroup };
