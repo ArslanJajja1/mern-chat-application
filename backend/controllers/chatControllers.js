a8f9b9929193802c8a87683aaffc405fe9b0d941
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-13 20:30:36 +0500

    add to group and remove from group controllers

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -89,6 +89,29 @@ const renameGroup = asyncHandler(async (req, res) => {
   }
 });
 
-const removeFromGroup = asyncHandler(async (req, res) => {});
-const addToGroup = asyncHandler(async (req, res) => {});
+const addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+  const added = await Chat.findByIdAndUpdate(chatId, { $push: { users: userId } }, { new: true })
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!added) {
+    res.status(404);
+    throw new Error('Chat not found');
+  } else {
+    res.json(added);
+  }
+});
+const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+  const removed = await Chat.findByIdAndUpdate(chatId, { $pull: { users: userId } }, { new: true })
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+  if (!removed) {
+    res.status(404);
+    throw new Error('Chat not found');
+  } else {
+    res.json(removed);
+  }
+});
+
 module.exports = { accessChat, fetchChats, createGroupChat, renameGroup, removeFromGroup, addToGroup };
