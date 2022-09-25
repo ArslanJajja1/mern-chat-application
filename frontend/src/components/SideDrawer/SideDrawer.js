192b3ba53fee40a5b34f49242002947b82314159
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-25 09:28:55 +0500

    side drawer user search functionality

--- frontend/src/components/SideDrawer/SideDrawer.js
+++ frontend/src/components/SideDrawer/SideDrawer.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
-import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  useToast,
+} from '@chakra-ui/react';
 import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from '../Modal/ProfileModal';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import ChatLoading from '../Loading/ChatLoading';
+import UserListItem from '../UserAvatar/UserListItem';
 const SideDrawer = () => {
   const [search, setSearch] = useState('');
   const [searchResult, setSearchResut] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
   const { user } = ChatState();
   const history = useHistory();
   const logoutHandler = () => {
@@ -16,47 +40,104 @@ const SideDrawer = () => {
     history.push('/');
     window.location.reload();
   };
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: 'Please Enter something in search',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-left',
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      setLoading(false);
+      setSearchResut(data);
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description: 'Failed to load search results',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+    }
+  };
+  const accessChat = (userId) => {};
   return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      bg="white"
-      w="100%"
-      p="5px 10px 5px 10px"
-      borderWidth="5px"
-    >
-      <Tooltip label="Search users for chat" hasArrow placement="bottom-end" textColor="white">
-        <Button variant="ghost">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <Text display={{ base: 'none', md: 'flex' }} px="4">
-            Search Users
-          </Text>
-        </Button>
-      </Tooltip>
-      <Text fontSize="2xl" fontFamily="work sans">
-        Mern Chat App
-      </Text>
-      <div>
-        <Menu>
-          <MenuButton p={1}>
-            <BellIcon fontSize="2xl" margin={1} />
-          </MenuButton>
-        </Menu>
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-            <Avatar size="sm" cursor="pointer" name={user.name} src={user.pic} />
-          </MenuButton>
-          <MenuList>
-            <ProfileModal user={user}>
-              <MenuItem>My Profile</MenuItem>
-            </ProfileModal>
-            <MenuDivider />
-            <MenuItem onClick={logoutHandler}>Logout</MenuItem>
-          </MenuList>
-        </Menu>
-      </div>
-    </Box>
+    <>
+      {' '}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search users for chat" hasArrow placement="bottom-end" textColor="white">
+          <Button variant="ghost" onClick={onOpen}>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <Text display={{ base: 'none', md: 'flex' }} px="4">
+              Search Users
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="work sans">
+          Mern Chat App
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" margin={1} />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar size="sm" cursor="pointer" name={user.name} src={user.pic} />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display="flex" pb={2}>
+              <Input placeholder="Search by name or email" mr={2} value={search} onChange={(e) => setSearch(e.target.value)} />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              <span>
+                {searchResult?.map((user) => (
+                  <UserListItem key={user._id} user={user} handleFuction={() => accessChat(user._id)} />
+                ))}
+              </span>
+            )}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
