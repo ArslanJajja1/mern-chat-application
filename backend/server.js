cfee9e79ebc7c3390fa0e1d1488c2ded7dc01baa
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-26 17:29:48 +0500

    added cors package

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const connectDB = require('./config/db');
 require('dotenv').config();
 const { chats } = require('./data/data');
@@ -9,6 +10,7 @@ const messageRoutes = require('./routes/messageRoutes');
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Express API is running ');
 });
@@ -17,5 +19,5 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server is running on port ', PORT));
