718cd6207fcd8f5011a02358f071a9735c6a7cac
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-28 20:21:42 +0500

    fixing group creation

--- frontend/src/components/Modal/GroupChatModal.js
+++ frontend/src/components/Modal/GroupChatModal.js
@@ -39,7 +39,7 @@ const GroupChatModal = ({ children }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/user?search=${search}`, config);
       setLoading(false);
       setSearchResult(data);
     } catch (error) {
@@ -71,7 +71,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.post(
-        `/api/chat/group`,
+        `${process.env.REACT_APP_API}/api/chat/group`,
         {
           name: groupChatName,
           users: JSON.stringify(selectedUsers.map((u) => u._id)),
