c0d9f60d443c60417e311146ca896994be1254c8
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-28 20:09:46 +0500

    fixing realtime notifications and typing feature in backend

--- backend/server.js
+++ backend/server.js
@@ -22,40 +22,44 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log('Server is running on port ', PORT));
 
-const io = require('socket.io')(server,{
-  pingTimeout:60000,
-  cors:{
-    origin: 'https://chat-app-by-arslan.web.app'
-  }
-})
 
-io.on('connection',(socket)=>{
-  console.log('connected to socket.io')
-  socket.on('setup',(userData)=>{
-    socket.join(userData._id)
-    console.log(userData._id)
-    socket.emit('connected')
-  })
-  socket.on('join chat',(room)=>{
-    socket.join(room)
-    console.log(`user joined room : ${room}`);
-  })
-  socket.on('typing',(room)=>{
-    socket.in(room).emit('typing')
-  })
-  socket.on('stop typing',(room)=>{
-    socket.in(room).emit('stop typing')
-  })
-  socket.on('new message',(newMessageRecieved)=>{
-    var chat = newMessageRecieved.chat
-    if (!chat.users) return console.log(`chat.users ${chat.users} not defined`)
-    chat.users.forEach(user=>{
-      if(user._id === newMessageRecieved.sender._id) return
-      socket.in(user._id).emit('message recieved',newMessageRecieved)
-    })
-  })
-  socket.off('setup',()=>{
-    console.log('user disconnected');
-    socket.leave(userData._id)
-  })
-})
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "https://chat-app-by-arslan.web.app",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
